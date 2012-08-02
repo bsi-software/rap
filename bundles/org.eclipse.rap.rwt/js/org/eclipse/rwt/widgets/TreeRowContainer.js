a3f0ea56bbe713590515d31450e644bffe8795d5
Stephan Leicht Vogt <Stephan.Leicht@bsiag.com>
2012-08-02 09:35:13 +0200

    NEW - bug 361796: [Table][Tree] Setting vertical alignment https://bugs.eclipse.org/bugs/show_bug.cgi?id=361796

--- bundles/org.eclipse.rap.rwt/js/org/eclipse/rwt/widgets/TreeRowContainer.js
+++ bundles/org.eclipse.rap.rwt/js/org/eclipse/rwt/widgets/TreeRowContainer.js
@@ -70,6 +70,7 @@ qx.Class.define( "org.eclipse.rwt.widgets.TreeRowContainer", {
         "columnCount" : 0,    
         "treeColumn" : 0,    
         "alignment" : [],   
+        "verticalAlignment" : [],
         "itemLeft" : [],   
         "itemWidth" : [],   
         "itemImageLeft" : [],   
